1a980308aa8650a789fc3eb77c1456f454e6cf96
Alberto Gonzalez <jgonzalez@ia.com.mx>
2021-04-26 17:27:32 -0500

    Add example

---
+++ IAConviva/IAConviva/IAConviva.h
@@ -0,0 +1,18 @@
+//
+//  IAConviva.h
+//  IAConviva
+//
+//  Created by José Alberto González Gordillo on 26/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IAConviva.
+FOUNDATION_EXPORT double IAConvivaVersionNumber;
+
+//! Project version string for IAConviva.
+FOUNDATION_EXPORT const unsigned char IAConvivaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IAConviva/PublicHeader.h>
+
+
---
+++ IAConviva/IAConviva/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  IAConviva
+//
+//  Created by José Alberto González Gordillo on 26/04/21.
+//
+
+import Foundation
+
+public class Service {
+    private init() { }
+
+    public static func doSomething() -> String {
+        return("welcome to conviva")
+    }
+}
